b75703df6adab7e13b3448860f0f5c33d830ab41
sanskruti chavan <sanskrutichavan45@gmail.com>
2025-06-06 10:59:50 +0530

    Day1
    Assignment Operator and  Relational  Operators-calculator

---
+++ Week1/Operaters.java
@@ -0,0 +1,36 @@
+public class Operaters {
+
+    public static void main(String[] args) {
+        //Operaters
+        int a = 20;
+        int b = 10;
+        System.out.println("Adition is: "+(a+b));
+
+        System.out.println("calculator :-");
+        //Add
+        System.out.println("Aditional is : "+(a+b));
+
+        //sub
+        System.out.println("Subtraction is : "+(a-b));
+
+         //Mul
+        System.out.println("Multiplication is : "+(a*b));
+
+         //Div
+        System.out.println("Division is : "+(a/b));
+
+         //Mod
+        System.out.println("Modules is : "+(a%b));
+
+System.out.println("_______________________________________________________________");
+        //Relational  Operators
+        System.out.println("Relational  Operators :- ");
+         System.out.println("eqals to : "+(a==b));
+         System.out.println("Not equal to : "+(a!=b));
+         System.out.println("Greater than : "+(a>b));
+         System.out.println("Less than : "+(a<b));
+         System.out.println("Greater or equal : "+(a>=b));
+         System.out.println("Less or equal : "+(a<=b));
+
+    }
+}
