eb5fe2a3477a788fbbf5a37b5d8cc68f3574af95
sanskruti chavan <sanskrutichavan45@gmail.com>
2025-06-10 09:37:37 +0530

    Add files via upload

---
+++ Week1/loopque.java
@@ -0,0 +1,10 @@
+public class loopque {
+    //in loop 100 times run 
+      public static void main(String[] args) {
+  
+     
+        for(int i=1;i<=100;i++){
+            System.out.println("run 100 times = "+i);
+        }
+    }
+}
