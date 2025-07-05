89dfd36938c8d291c70f12ea7d33d9d22bcc70f9
sanskruti chavan <sanskrutichavan45@gmail.com>
2025-07-05 16:37:05 +0530

    Add files via upload

--- bubblesort.java
+++ bubblesort.java
@@ -31,7 +31,7 @@ public class bubblesort {
             }
         } else {
             System.out.println("Sorted array:");
-            for (int i = 0; i < arr.length - 1; i++) {
+            for (int i = 0; i < arr.length; i++) {
                 System.out.print(arr[i] + " ");
             }
         }
